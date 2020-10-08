13400ee5a939f877ae66aaeb9ac117d0c8853164
boyang <wangboyang1991@gmail.com>
2020-10-08 15:11:09 +0800

    FEATURE: remove server to another repo

--- server/app/router.ts
+++
@@ -1,8 +0,0 @@
-import { Application } from 'egg';
-
-export default (app: Application) => {
-  const { controller, router } = app;
-
-  router.get('/api/wechat/echo', controller.home.wechatEcho);
-  router.get('/api', controller.home.index);
-};
--- server/deploy.sh
+++
@@ -1,9 +0,0 @@
-rsync -arvz --exclude=node_modules --exclude=run --exclude=logs --progress ./ root@101.200.34.182:/data/server-apps/lanting/server
-
-ssh -t root@101.200.34.182 '
-cd /data/server-apps/lanting/server
-npm install
-npm run ci
-npm run stop
-npm run start
-'
