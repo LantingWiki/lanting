e06a169bf74d70ff56b10e6f8312900059aeaf7c
boyang <wangboyang1991@gmail.com>
2020-10-12 15:34:33 +0800

    FEATURE: 支持公众号转载

--- server/clipper/cli/single-file-browser.js
+++ server/clipper/cli/single-file-browser.js
@@ -3,21 +3,20 @@
 /* eslint-disable no-var */
 /* eslint-disable no-console */
 res = { title: document.querySelector('.rich_media_title').innerText };
-// xPost = Array.from(document.querySelectorAll('.original_primary_card_tips'))
-//   .find(n => n.innerText.startsWith('The following article is from'));
-// let m;
-// if (xPost) {
-//   // 转载 x-posting
-//   m = xPost.innerText.match(/^The following article is from (.*?) Author (.*)$/);
-//   res.author = m[2];
-//   res.publisher = m[1];
-// } else {
-authorNode = document.querySelector('span.rich_media_meta.rich_media_meta_text');
-if (authorNode) {
-  res.author = authorNode.innerText;
+xPost = document.querySelector('.original_primary_card_tips');
+
+if (xPost) {
+  // 转载 x-posting
+  m = xPost.innerText.match(/^The following article is from (.*?) Author (.*)$/);
+  res.author = m[2];
+  res.publisher = m[1];
+} else {
+  authorNode = document.querySelector('span.rich_media_meta.rich_media_meta_text');
+  if (authorNode) {
+    res.author = authorNode.innerText;
+  }
+  res.publisher = document.querySelector('#js_name').innerText;
 }
-res.publisher = document.querySelector('#js_name').innerText;
-// }
 dt = document.querySelector('#publish_time').innerText;
 if (dt === 'Yesterday' || dt === 'Today' || dt.includes('days ago') || dt.includes('week ago')) {
   dt = new Date().toISOString().substring(0, 7);
