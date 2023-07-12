e9ccc2f66826937dd1752d777966fefe4a8720e7
boyang <wangboyang1991@gmail.com>
2023-07-12 17:58:02 +0800

    you can't import from outside of /api

--- api/search-keyword/read.ts
+++ api/search-keyword/read.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { kv } from "@vercel/kv";
-import { allowCors } from "../../src/utils/request";
+import { allowCors } from "../utils/echo";
 
 export default allowCors(async function handler(
   request: VercelRequest,
