db1272ba0d4807f231bc9d34192ffc1cb41fa54e
boyang <wangboyang1991@gmail.com>
2023-07-12 19:04:03 +0800

    use stricker origin header -> req.origin. Likely will not work

--- api/likes/create.ts
+++ api/likes/create.ts
@@ -3,9 +3,9 @@ import { kv } from "@vercel/kv";
 
 export const allowCors = (fn: (arg0: any, arg1: any) => any) => async (req: VercelRequest, res: VercelResponse) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  // res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
--- api/search-keyword/create.ts
+++ api/search-keyword/create.ts
@@ -3,19 +3,19 @@ import { kv } from "@vercel/kv";
 
 export const allowCors = (fn: (arg0: any, arg1: any) => any) => async (req: VercelRequest, res: VercelResponse) => {
   res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  // res.setHeader('Access-Control-Allow-Origin', '*')
   // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   )
   if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
+    res.status(200).end();
+    return;
   }
-  return await fn(req, res)
+  return await fn(req, res);
 }
 
 export default allowCors(async function handler(
